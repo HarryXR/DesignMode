a7d9bdd6d15645a195082993674f5b912ca8d743
xiarui@maianer.com <xiarui@mainaer.com>
2017-02-11 14:38:51 +0800

    add singleton

---
+++ app/src/main/java/com/harry/design/single/SingleTon.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 20015 MaiNaEr All rights reserved
+ */
+package com.harry.design.single;
+
+/**
+ * 单例
+ * 一个类只有一个实例
+ *
+ * @author Harry
+ * @date 2017/2/11.
+ */
+public class SingleTon {
+
+    private static SingleTon instance;
+
+    private SingleTon() {
+
+    }
+
+    public static synchronized SingleTon getInstance() {
+        /*
+        双重检查加锁
+        只有第一次调用时才会同步
+         */
+        if (instance == null) {
+            synchronized (SingleTon.class) {
+                if (instance == null) {
+                    instance = new SingleTon();
+                }
+            }
+        }
+        return instance;
+    }
+}
